41159512efca83885aad7833b9ab3e293bf61639
omar22326026 <121298458+omar22326026@users.noreply.github.com>
2023-12-14 20:01:57 +0600

    uncommented important lines

--- main.py
+++ main.py
@@ -17,29 +17,29 @@ bacteria_names = list(
 
 modules = ["M00175", "M00528", "M00529", "M00530", "M00531", "M00804", "M00973"]
 
-# orthologies = list(
-#     map(
-#         lambda x: kegg_crawl.get_module_orthologies(x),
-#         modules
-#     )
-# )
-# orthologies = sum(orthologies, [])
+orthologies = list(
+    map(
+        lambda x: kegg_crawl.get_module_orthologies(x),
+        modules
+    )
+)
+orthologies = sum(orthologies, [])
 
-# bacterial_orthologies = []
+bacterial_orthologies = []
 
-# for each in orthologies:
-#     taxonomy = kegg_crawl.get_orthology_taxonomy(each)
-#     # print(taxonomy)
-#     for organism in taxonomy:
-#         if organism in bacteria_names:
-#             bacterial_orthologies.append(each)
-#             break
+for each in orthologies:
+    taxonomy = kegg_crawl.get_orthology_taxonomy(each)
+    # print(taxonomy)
+    for organism in taxonomy:
+        if organism in bacteria_names:
+            bacterial_orthologies.append(each)
+            break
 
 # print(bacterial_orthologies)
 
-# nonbacterial_orthologies = [x for x in orthologies if x not in bacterial_orthologies]
+nonbacterial_orthologies = [x for x in orthologies if x not in bacterial_orthologies]
 
-nonbacterial_orthologies = ['K10534', 'K17877']
+# nonbacterial_orthologies = ['K10534', 'K17877']
 
 reactions = list(
     map(
